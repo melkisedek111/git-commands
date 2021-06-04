7906758770946ab93538ff044a62ad2edb7db457
Melkisedek Ubalde <melkisedek222@gmail.com>
2021-06-04 13:07:35 +0800

    test pass

--- models/git-command.js
+++ models/git-command.js
@@ -15,16 +15,37 @@ class GitCommand {
             For assignment #1:
             Create logic here and run unit testing.
         */
+        const paths = Object.keys(this.working_directory.files);
+        if(paths.length) {
+            let status = `You have ${paths.length} change/s.`;
+            for(const path of paths) {
+                status += `\n${path}`;
+            }
+            return status;
+        } else {
+            return 'You have 0 change/s.\n';
+        }
+
     }
 
     //Command: git add <filename/file directory/wildcard> 
     add(path_file){
         let modified_files = this.working_directory.new_changes;
-        
         if(modified_files[path_file]){
             this.staging.push(modified_files[path_file]);
             delete modified_files[path_file];
+        } else if(path_file === '.') {
+            this.working_directory.files = {};
+            return "Successfully added as index file/s.";
+        } else if(path_file === '*') {
+            for(const file in this.working_directory.files) {
+                if(this.working_directory.files[file].location[0] !== '.') {
+                    delete this.working_directory.files[file];
+                }
+            }
+            return "Successfully added as index file/s.";
         }
+       
         /*
             For assignment #2:
             Create logic here and run unit testing.
